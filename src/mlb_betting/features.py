e28987566d229607c9dd3a9397b199a0c41e1552
Gerard Kirwin <48104932+gerardrobertkirwin@users.noreply.github.com>
2026-01-08 19:54:30 +0000

    Update features.py

--- src/mlb_betting/features.py
+++ src/mlb_betting/features.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import os
 
 def create_team_centric_df(df_master: pd.DataFrame) -> pd.DataFrame:
     """
@@ -80,3 +79,75 @@ def calculate_rolling_features(df_long, window_size=10):
 
     return df_features
 
+def calculate_advanced_features(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Calculates rest days and Log5 probability
+    """
+    
+    df_adv = df.copy()
+
+    # Rest Days
+    # If played yesterday (May 2 - May 1) = 1 day.
+    df_adv['rest_days'] = df_adv.groupby('team')['date'].diff().dt.days
+    # Fill the first game of the season with huge rest (e.g., 5 days)
+    df_adv['rest_days'] = df_adv['rest_days'].fillna(5)
+
+    #Create a small lookup table
+    opponent_stats = df_adv[['date', 'team', 'rolling_pythag_win_pct']].copy()
+    opponent_stats = opponent_stats.rename(columns={
+        'team': 'opponent', # Rename so we can join on this
+        'rolling_pythag_win_pct': 'opp_pythag_win_pct'
+    })
+
+    df_adv = pd.merge(
+        df_adv,
+        opponent_stats,
+        on=['date', 'opponent'],
+        how='left'
+    )
+
+    # Log5
+    A = df_adv['rolling_pythag_win_pct']
+    B = df_adv['opp_pythag_win_pct']
+
+    # We use a safe division formula
+    numerator = A * (1 - B)
+    denominator = (A * (1 - B)) + (B * (1 - A))
+
+    df_adv['log5_prob'] = numerator / denominator
+
+    return df_adv
+
+def finalize_training_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Drops early season NaNs (pre game 10) and encodes team IDs
+    """
+    df_train = df.copy()
+
+    # 1. Drop the Warm-Up Period
+    df_train = df_train.dropna(subset=['rolling_10_runs_scored', 'log5_prob'])
+    
+    # 2. Encode Team Names for ML process
+    df_train['team_code'] = df_train['team'].astype('category').cat.codes
+    df_train['opponent_code'] = df_train['opponent'].astype('category').cat.codes
+
+    # 3. Final Feature Selection
+    features = [
+        'is_home',
+        'rest_days',
+        'log5_prob',
+        'rolling_10_runs_scored',
+        'rolling_10_runs_allowed',
+        'rolling_10_hits',
+        'rolling_10_errors',
+        'rolling_pythag_win_pct',
+        'opp_pythag_win_pct',
+        'team_code',
+        'opponent_code',
+        'moneyline_closing' # We don't train on this, but we need it for the betting simulation later
+    ]
+
+    # The Target
+    target = 'result' # 1 = Win, 0 = Loss
+
+    return df_train[features + [target, 'date', 'team', 'opponent']]
