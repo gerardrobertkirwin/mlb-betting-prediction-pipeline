6ecbc0ff91bd566999d5b7c33d53a7cc97f9e101
Gerard Kirwin <48104932+gerardrobertkirwin@users.noreply.github.com>
2026-01-11 00:02:02 +0000

    Update modeling.py

--- src/mlb_betting/modeling.py
+++ src/mlb_betting/modeling.py
@@ -49,15 +49,80 @@ class BayesianBettingModel:
     """
     Loads the model and generates probability predictions for new data.
     """
-    # 1. Load Model if needed
+    # Load Model if needed
     if self.trace is None:
         if not os.path.exists(self.model_path):
             raise FileNotFoundError(f"Model not found at {self.model_path}")
         print(f"Loading model from {self.model_path}...")
         self.trace = az.from_netcdf(self.model_path)
 
+    X = df_new[feature_cols].values
     
-    with bayesian_model:
-      ppc = pm.sample_posterior_predictive(trace, var_names=["y_obs"])
+    X_imputed = self.imputer.transform(X)
+    X_scaled = self.scaler.transform(X_imputed)
 
+    with pm.Model() as model:
+        alpha = pm.Normal("alpha", mu=0, sigma=1)
+        betas = pm.Normal("betas", mu=0, sigma=1, shape=X_scaled.shape[1])
+
+        mu = alpha + pm.math.dot(X_scaled, betas)
+        theta = pm.math.sigmoid(mu)
+    
+        y_obs = pm.Bernoulli("y_obs", p=theta, shape=len(X_scaled))
+
+        ppc = pm.sample_posterior_predictive(self.trace, var_names=["y_obs"])
+    
       bayesian_probs = ppc.posterior_predictive['y_obs'].mean(dim=["chain", "draw"]).values
+
+
+def simulate_betting(df, threshold=0.05, stake=100):
+    """
+    Simulates betting $100 whenever our model sees an edge > 5%.
+    """
+    sim = df.copy()
+
+    # 1. Calculate probability from odds
+    def us_odds_to_prob(odds):
+        if pd.isna(odds): return np.nan
+        if odds > 0:
+            return 100 / (odds + 100)
+        else:
+            return (-odds) / (-odds + 100)
+
+    sim['vegas_prob'] = sim['moneyline_closing'].apply(us_odds_to_prob)
+
+    # 2. Find the edge
+    sim['edge'] = sim['my_prob'] - sim['vegas_prob']
+
+    # 3. Place bets
+    sim['bet_placed'] = sim['edge'] > threshold
+
+    #4. Calculate profit and loss
+    def calculate_pnl(row):
+        if not row['bet_placed']: return 0
+
+        # Convert US odds to Decimal multiplier for payout
+        if row['moneyline_closing'] > 0:
+            decimal_odds = 1 + (row['moneyline_closing'] / 100)
+        else:
+            decimal_odds = 1 + (100 / -row['moneyline_closing'])
+
+        if row['result'] == 1:
+            return stake * (decimal_odds - 1)
+        else:
+            return -stake
+ 
+      
+    sim['pnl'] = sim.apply(calculate_pnl, axis=1)
+    
+    # METRICS
+    total_bets = sim['bet_placed'].sum()
+    total_profit = sim['pnl'].sum()
+    roi = (total_profit / (total_bets * stake)) * 100 if total_bets > 0 else 0
+    
+    return {
+    "total_bets": int(total_bets),
+    "total_profit": float(total_profit),
+    "roi": float(roi)
+    }
+    
